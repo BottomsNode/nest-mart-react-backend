c46ca040e8327a72aeb09b6ef7ce2dd85fc143b2
Nishit Shivdasani <120126924+BottomsNode@users.noreply.github.com>
2025-07-21 16:14:44 +0530

    added the user name to payload - auth.service.ts

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -23,6 +23,7 @@ export class AuthService {
       return {
         id: user.id,
         email: user.email,
+        name: user.name,
         role: {
           name: user.role.name,
           permissions: user.role.permissions.map((p) => p.name),
@@ -43,6 +44,7 @@ export class AuthService {
     const payload = {
       id: user.id,
       email: user.email,
+      name:user.name,
       role: user.role.name,
       permissions: user.role.permissions,
     };
