900fee5d7de3a3304185e0487119fafd2666037e
nishit <nishit.shivdasani@esparkbizmail.com>
2025-07-25 17:56:45 +0530

    changes in swagger config

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,11 +1,11 @@
 import { DocumentBuilder } from '@nestjs/swagger';
-import { DB_HOST, DB_NAME, DB_PORT } from 'src/common/secrets/secrets.keys';
+import { DB_HOST, DB_NAME, PORT } from 'src/common/secrets/secrets.keys';
 
 export const swaggerConfig = new DocumentBuilder()
   .setTitle('NestMart API Documentation')
   .setDescription('Swagger For API Documentation')
   .setVersion('1.0')
-  .addServer(`http://${DB_HOST}:${DB_PORT}/`, `${DB_NAME} Local environment`)
+  // .addServer(`http://${DB_HOST}:${PORT}/`, `${DB_NAME} Local environment`)
   .addTag(`List of ${DB_NAME} API's`)
   .addBearerAuth()
   .build();
